6ea603f6c37867986633f62d6c50f0730e448ba0
Tihjs137 <thijsdejong21@gmail.com>
2019-03-21 12:28:10 +0100

    Added Catkin_ws to root

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/thijs/Turbine-inspector/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/thijs/Turbine-inspector/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/thijs/Turbine-inspector/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/thijs/Turbine-inspector/src:$ROS_PACKAGE_PATH"
